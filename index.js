116858e1a60a04a084e0dc58ea055a93a9609714
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2022-05-20 18:24:26 -0300

    apsotila8-redux fim da secao2.8 2.0

--- index.js
+++ index.js
@@ -26,6 +26,25 @@ const solicitarCashback = (nome,valor) =>{
 }
 
 //funcão redux
-const historicoDePedidosDeCashBacks = (historicoDePedidosDeCashBacks = []) => {
+const historicoDePedidosDeCashBacksReducer = (historicoDePedidosDeCashBacksAtual = [], acao) => {
+    if (acao.type === "SOLICITAR_CASHBACK") {
+        return{
+            ...historicoDePedidosDeCashBacksAtual,
+            acao,payload
+        }
+    }
+    return historicoDePedidosDeCashBacksAtual
 
+}
+
+//outra função redux
+
+const caixaReducer = (dinheiroEmCaixa = 0, acao) => {
+    if(acao.type === "SOLICITAR_CASHBACK"){
+        dinheiroEmCaixa = dinheiroEmCaixa - acao.payload.valor
+    }
+    else if(acao.type === "CRIAR_CONTRATO"){
+        dinheiroEmCaixa = dinheiroEmCaixa + acao.payload.taxa
+    }
+    return dinheiroEmCaixa
 }
